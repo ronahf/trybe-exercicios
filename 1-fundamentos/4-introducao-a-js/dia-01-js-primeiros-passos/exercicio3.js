b2ca537b4f10618b53fc9c74a1580103482f5113
ronahf <ronaldohfrancisco@gmail.com>
2022-09-21 21:45:56 -0300

    exercicio 4

--- 1-fundamentos/4-introducao-a-js/dia-01-js-primeiros-passos/exercicio3.js
+++ 1-fundamentos/4-introducao-a-js/dia-01-js-primeiros-passos/exercicio3.js
@@ -1,3 +1,5 @@
+//Utilize if/else para fazer um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+
 let x = 10;
 let y = 10;
 let z = 10;
