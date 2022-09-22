ba1212c01744d4202dd22f8641d4b8c4f0a3c049
ronahf <ronaldohfrancisco@gmail.com>
2022-09-21 21:41:11 -0300

    exercicio 3

---
+++ 1-fundamentos/4-introducao-a-js/dia-01-js-primeiros-passos/exercicio3.js
@@ -0,0 +1,16 @@
+let x = 10;
+let y = 10;
+let z = 10;
+
+if (x > y && x > z){
+    console.log(x);
+}
+else if (y > x && y > z){
+    console.log(y);
+}
+else if (z >= y & z >= x){
+    console.log(z);
+}
+else{
+    console.log(x);
+}
