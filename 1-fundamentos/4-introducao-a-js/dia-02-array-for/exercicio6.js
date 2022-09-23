5500d527bdf86c92a54e026b363f0c1bb63b354d
ronahf <ronaldohfrancisco@gmail.com>
2022-09-23 11:48:20 -0300

    último ex. da aula 4.2

--- 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio6.js
+++ 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio6.js
@@ -1,11 +1,17 @@
 //Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
-
-let elementosCriados;
 let array = [];
-
 
 for (let index = 1; index <= 25; index += 1) {
     array.push(index);
 }
-console.log(array)
 
+console.log(array);
+
+//Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2;
+let arrayDividePorDois = [];
+
+for (let index = 0; index < 25; index += 1) {
+    arrayDividePorDois.push((array[index]/2));
+}
+
+console.log(arrayDividePorDois);
