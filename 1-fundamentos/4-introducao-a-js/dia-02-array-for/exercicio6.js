25d1697ba6f31f86f95f07e32c29cb38ef058ee9
ronahf <ronaldohfrancisco@gmail.com>
2022-09-22 21:40:41 -0300

    ex 6

---
+++ 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio6.js
@@ -0,0 +1,11 @@
+//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+let array = [];
+let elemento;
+
+for (let index = 0; index < 25; index += 1) {
+    array.push(elemento[index]);
+    elemento= elemento + 1; 
+}
+console.log(array);
+
