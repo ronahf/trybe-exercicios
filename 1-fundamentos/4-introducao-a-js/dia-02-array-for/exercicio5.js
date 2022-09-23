1a25510c7dde122141515607caec819a81ea5b34
ronahf <ronaldohfrancisco@gmail.com>
2022-09-22 21:02:09 -0300

    impares completo

--- 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio5.js
+++ 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio5.js
@@ -1,11 +1,14 @@
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
 
 let numbers = [0, 2, 4, 6, 77, 8, 1, 2, 35, 1];
-let impares=0;
+let quantidadeDeImpares=0;
+let imparesEncontrados = [];
 
 for (let index = 0; index < numbers.length; index += 1){
     if ((numbers[index]%2) !== 0){
-        impares = impares + 1;
+        quantidadeDeImpares = quantidadeDeImpares + 1;
+        imparesEncontrados.push(numbers[index]);
     }
 }
-console.log(impares);
+console.log(quantidadeDeImpares);
+console.log(imparesEncontrados);
