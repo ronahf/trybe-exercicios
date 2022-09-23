60f05668a481c9f3c5afd9b838e5cc1595bd2151
ronahf <ronaldohfrancisco@gmail.com>
2022-09-22 21:17:01 -0300

    Menor número da array

--- 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio4.js
+++ 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio4.js
@@ -1,2 +1,10 @@
-//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+////Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+let numbers = [5, 9, 3, 100, 70, 8, 100, 2, 35, 27];
+let menor=0;
 
+for (let index=0; index<numbers.length; index += 1){
+    if (numbers[index] < numbers[index+1]){
+        menor = numbers[index];
+    }
+}
+console.log(menor);
--- 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio5.js
+++ 1-fundamentos/4-introducao-a-js/dia-02-array-for/exercicio5.js
@@ -1,6 +1,6 @@
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
 
-let numbers = [0, 2, 4, 6, 77, 8, 1, 2, 35, 1];
+let numbers = [0, 2, 4, 6, 2, 8, 10, 2, 350, 10];
 let quantidadeDeImpares=0;
 let imparesEncontrados = [];
 
@@ -10,5 +10,10 @@ for (let index = 0; index < numbers.length; index += 1){
         imparesEncontrados.push(numbers[index]);
     }
 }
+
+if (quantidadeDeImpares === 0){
+    console.log('Nenhum valor ímpar encontrado!')
+}
+
 console.log(quantidadeDeImpares);
 console.log(imparesEncontrados);
