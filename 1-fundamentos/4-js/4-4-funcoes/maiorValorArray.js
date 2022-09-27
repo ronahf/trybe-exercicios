cab7f27071cda34f52125af1ed00fe0b4db64ab9
ronahf <ronaldohfrancisco@gmail.com>
2022-09-27 06:43:32 -0300

    ajuste maior valor array

--- 1-fundamentos/4-js/4-4-funcoes/maiorValorArray.js
+++ 1-fundamentos/4-js/4-4-funcoes/maiorValorArray.js
@@ -1,4 +1,19 @@
 //Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
 //Array de teste: [2, 3, 6, 7, 10, 1];.
 //Valor esperado no retorno da função: 4.
 
+let array = [10, 10000, 1, 7, -100, 99999];
+let maiorElemento = [];
+let indexMaiorElemento = [];
+
+function biggest (array){
+    for (let index = 0; index < array.length; index += 1){
+        if (array[index] > maiorElemento){
+            maiorElemento = array[index];
+            console.log(index);
+        } 
+    } 
+    return maiorElemento;
+}
+console.log(biggest(array));
