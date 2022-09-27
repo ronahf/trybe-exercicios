4dee68fc51989bd70dd12b418332bc8385087fc8
ronahf <ronaldohfrancisco@gmail.com>
2022-09-27 11:01:46 -0300

    verifica fim da palavra

---
+++ 1-fundamentos/4-js/4-4-funcoes/verificaFimPalavra.js
@@ -0,0 +1,24 @@
+//Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+
+//Valor de teste: 'trybe' e 'be'
+//Valor esperado no retorno da função: true
+
+//verificaFimPalavra('joaofernando', 'fernan');
+//Retorno esperado: false
+
+function wordAndEnding (word, ending){
+    word = word.split(''); //transforma as strings em arrays!
+    ending = ending.split(''); // transforma as strings em arrays!
+
+    let comparation = true;
+
+    for (let index = 0; index < ending.length; index += 1) {
+        if (word[word.length - ending.length + index] !== ending[index]){
+            comparation = false;
+        }
+    }
+    return comparation;
+}
+
+console.log(wordAndEnding('ronaldo', 'aldo'));
+console.log(wordAndEnding('ronaldo', 'rona'));
