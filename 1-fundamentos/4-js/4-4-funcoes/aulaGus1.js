5713e623d83043bd337d1fa7265269acf73f6a2e
ronahf <ronaldohfrancisco@gmail.com>
2022-09-27 13:36:39 -0300

    atualizando soma

--- 1-fundamentos/4-js/4-4-funcoes/aulaGus1.js
+++ 1-fundamentos/4-js/4-4-funcoes/aulaGus1.js
@@ -1,7 +1,5 @@
 //função que soma todos os itens de um array.
 
-
-
 function sumFunction (array){ //bigode = chave!
     let result = 0;
     for (let index = 0; index < array.length; index += 1){
@@ -11,4 +9,4 @@ return result;
 }
 
 
-console.log(sumFunction([1, 2, 3, 4, 5, 1000]));
+console.log(sumFunction([1, 2, 3, 4, 5, 1000]))
