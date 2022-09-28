5276c0c1bbc067d27ee97ece8e73b84ad3dd8701
ronahf <ronaldohfrancisco@gmail.com>
2022-09-28 15:45:11 -0300

    alterando parâmetros do html

---
+++ 1-fundamentos/5-dom-eventos-storage/1domEseletores/aula5/script.js
@@ -0,0 +1,5 @@
+document.getElementById('page-title').innerText = '7ven & Pulp Fiction';
+
+document.getElementById('second-paragraph').innerText = 'Tarantino é um dos maiores escritores e diretores que a sétima arte já viu!';
+
+document.getElementById('subtitle').innerText = 'Maravilhas das telinhas && || telonas'
