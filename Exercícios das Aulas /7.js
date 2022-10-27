a37f7db22305fd359bab1d56d4c22c04b77667ff
ronahf <ronaldohfrancisco@gmail.com>
2022-10-27 11:47:54 -0300

    7

--- Exercícios das Aulas /7.js
+++ Exercícios das Aulas /7.js
@@ -1,3 +1,13 @@
 const { books } = require("./books");
 
 // Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+const authorWith3DotsOnName = () => {
+    return books.find((book) => 
+    book.author.name[1] === '.' &&
+    book.author.name[4] === '.' &&
+    book.author.name[7] === '.' 
+    ).name
+}
+
+console.log(authorWith3DotsOnName());
