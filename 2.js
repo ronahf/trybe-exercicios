1ec6bda09e512f38ae20dc09c051aea8660b2ca9
ronahf <ronaldohfrancisco@gmail.com>
2022-10-28 00:41:49 -0300

    3 dia 8.4

--- 2.js
+++ 2.js
@@ -1,16 +1,28 @@
 const books = require('./books');
 
-//Crie uma string com os nomes de todas as pessoas autoras.
-
+// Crie uma string com os nomes de todas as pessoas autoras.
 const reduceNames = () => {
     return books.reduce((acc, curr) => acc + `${curr.author.name}, `, '')
 }
-console.log(reduceNames());
+// console.log(reduceNames());
 
 
+// Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+const averageAge = () => {
+    return books.map((book) => book.releaseYear - book.author.birthYear)
+    .reduce((acc, curr) => (acc + curr))/(books.length);
+}
+// console.log(averageAge());
 
 
+// Encontre o livro com o maior nome.
+const longestNamedBook = () => {
+    return books.reduce((biggestBook, currentBook) => {
+        if (currentBook.name.length > biggestBook.name.length){
+            return currentBook;
+        }
+        return biggestBook;
+    });
+}
 
-
-
-const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+console.log(longestNamedBook());
--- 3.js
+++ 3.js
@@ -1,10 +1,31 @@
-const books = require('./books');
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
 
-// Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
-
-const averageAge = () => {
-    return books.map((book) => book.releaseYear - book.author.birthYear)
-    .reduce((acc, curr) => (acc + curr))/(books.length);
+  // retorne a quantidade de vezes que retorna a letra A ou a
+  
+  const containsA = () => {
+    let totalDeLetrasA = 0;
+    names.forEach((name) => {
+        const letras = name.split('');
+        console.log(letras);
+        totalDeLetrasA += letras.reduce((letrasAa, currentLetter) => {
+            if (currentLetter === 'a' || currentLetter === 'A') {
+                return letrasAa + 1
+            }
+            return letrasAa;
+        }, 0)
+    }); 
+    return totalDeLetrasA;
 }
 
-console.log(averageAge());
+// variável que contém o total de letras que nos interessa, ou seja, total;
+// para cada palavra, contar quantas vezes a letra A ou a aparecem;
+// percorrer o array que contém os nomes, com o forEach
+// a string serve para quebrar a string em array com cada letra, cada letra é um elemento do array letras no caso.
+
+  console.log(containsA());
+
