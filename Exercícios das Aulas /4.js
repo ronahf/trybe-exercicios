0840e6875d78cee05c58eb23dbc2982fe387319b
ronahf <ronaldohfrancisco@gmail.com>
2022-10-28 12:58:25 -0300

    fim da aula 8.4

--- Exercícios das Aulas /4.js
+++ Exercícios das Aulas /4.js
@@ -4,6 +4,31 @@
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
+
+
+
+
+
+
+
+
+
+
+
+
+// const expected = [
+    // {   
+        //     name: 'Pedro Henrique', 
+        //     average: 7.8 
+        // },
+        // { 
+//     name: 'Miguel', 
+//     average: 9.2 
+// },
+//     name: 'Maria Clara', 
+//     average: 8.8 
+// },
+// ];
 const studentAverage = () => {
     return nomeComMedia = students.map((student, index) => (
     {
@@ -12,22 +37,6 @@ const studentAverage = () => {
     }
     ));
 }
-
 console.log(studentAverage());
 
-
-
-// const expected = [
-// {   
-//     name: 'Pedro Henrique', 
-//     average: 7.8 
-// },
-// { 
-//     name: 'Miguel', 
-//     average: 9.2 
-// },
 // {
-//     name: 'Maria Clara', 
-//     average: 8.8 
-// },
-// ];
