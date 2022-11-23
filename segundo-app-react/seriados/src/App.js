764606f67c0eecae1d8868d632922b1ac64e5e84
ronahf <ronaldohfrancisco@gmail.com>
2022-11-23 19:06:13 -0300

    exercise series

---
+++ segundo-app-react/seriados/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CardList from './components/CardList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='header'>Series</h1>
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
---
+++ segundo-app-react/seriados/src/data.js
@@ -0,0 +1,22 @@
+const series = [ 
+    {
+        id: 1,
+        release: 2011,
+        name: 'Supernatural',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQezAJ77CsmxyfQLTSE07RNpZza6TiKGYgT2w&usqp=CAU',
+        characters: ['Sei Lá', 'ludi'],
+        genre: 'Drama',
+        rate: '5',
+    },
+    {
+        id: 2,
+        release: 2012,
+        name: 'Breaking Bad',
+        image: 'https://aventurasnahistoria.uol.com.br/media/_versions/curiosidades/breakin_bad_heisenberg_capa_widelg.jpg',
+        characters: ['WW', 'Skyler', 'Gus'],
+        genre: 'Dorgas',
+        rate: '10',
+    }
+];
+
+export default series;
